e4df190870aca889437b94dbc1b3c3484d491018
Makenalyn <makenalynton@gmail.com>
2023-10-05 03:21:02 -0700

    Added more libraries as per the function calls

--- main.h
+++ main.h
@@ -5,5 +5,11 @@
 #include <fcntl.h>
 #include <stddef.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/stat.h>
 
 #endif
